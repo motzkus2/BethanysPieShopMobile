7f8513291bfa6cd7d54fc15bb264ca6549e4fd4c
Thomas Arpe Motzkus <akimbobigmac@gmail.com>
2020-08-24 13:15:30 +0200

    Add project files.

---
+++ BethanysPieShopMobile/BethanysPieShopMobile/NavigationPageView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using BethanysPieShopMobile.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BethanysPieShopMobile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NavigationPageView : ContentPage
+    {
+        public NavigationPageView()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnBasket_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new BasketView());
+        }
+
+        private async void btnContactUs_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ContactusView());
+        }
+
+        private async void btnHome_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new HomePageView());
+        }
+
+        private async void btnPiesOverview_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PieOveriewView());
+        }
+    }
+}
